fe8ddf3febedeaa56da96b451fc38a9150fe2bec
alfio <alfio@alfio-System-Product-Name>
2013-11-27 16:09:52 -0500

    added comment

--- runrightfast-commons/src/main/groovy/co/runrightfast/commons/UUIDUtils.groovy
+++ runrightfast-commons/src/main/groovy/co/runrightfast/commons/UUIDUtils.groovy
@@ -2,6 +2,10 @@ package co.runrightfast.commons
 
 abstract class UUIDUtils {
 
+	/**
+	 * 
+	 * @return uuid with '-' stripped out to conserve bytes
+	 */
 	def static String uuid() {
 		UUID.randomUUID().toString().replace('-', '');
 	}
